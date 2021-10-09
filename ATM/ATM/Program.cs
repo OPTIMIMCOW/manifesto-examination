60b5f62001a7ae158c2fe622e2e3050ea1598cab
OPTIMIMCOW <kenallan18@hotmail.com>
2021-10-09 14:54:08 +0100

    Planning.

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -6,7 +6,20 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // assume json input (an array representing each row)
+            // deserialise json into an array
+            // scan through line by line: 
+            // make global variable for total cash in program.cs
+            // create class for each user:
+            // 1) variable total ballance available
+            // 2) variable to hold account number
+            // 3) variable for correct pin number
+            // 4) method to authenticate pin number
+            // authenticate pin, if pass then read through transactions and handle each sequentially else return
+            // 5) method to check if withdrawal is ok if not return 
+            // 6) method to update the ballance
+            // when blank line hit, return final balance.  repeat above steps
+            // when row == arr.length end script.
         }
     }
 }
