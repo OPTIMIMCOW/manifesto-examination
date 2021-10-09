c4d448529b0f14f1b4136717d3661ca645a29d6e
OPTIMIMCOW <kenallan18@hotmail.com>
2021-10-09 19:35:44 +0100

    Updated JSON to example data. Implemented code but needs refactor.

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -9,28 +9,104 @@ namespace ATM
     {
         static void Main(string[] args)
         {
-            // assume json input (an array representing each row)
-            // deserialise json into an array
-            // scan through line by line: 
-            // make global variable for total cash in program.cs
-            // create class for each user:
-            // 1) variable total ballance available
-            // 2) variable to hold account number
-            // 3) variable for correct pin number
-            // 4) method to authenticate pin number
-            // authenticate pin, if pass then read through transactions and handle each sequentially else return
-            // 5) method to check if withdrawal is ok if not return 
-            // 6) method to update the ballance
-            // when blank line hit, return final balance.  repeat above steps
-            // when row == arr.length end script.
-            var JsonFileLocation = @"..\..\..\TestFile.json";
-            DeserialiseJson(JsonFileLocation);
+            try
+            {
+                var jsonFileLocation = @"..\..\..\TestFile.json";
+                var inputInformation = DeserialiseJson(jsonFileLocation);
 
-        }
-        public static List<string> DeserialiseJson(String path)
-        {
-            var inputJson = File.ReadAllText(path);
-            return JsonConvert.DeserializeObject<List<string>>(inputJson);
+                var rowNumber = 0;
+                var atmFunds = int.Parse(inputInformation[rowNumber]);
+                rowNumber++;
+                if (inputInformation[rowNumber] != "")
+                {
+                    throw new Exception($"Input Data Failure. Row{rowNumber} was expexted to be empty but was {inputInformation[rowNumber]}");
+                }
+                rowNumber++;
+
+                // start Interaction as a function
+                while (rowNumber != inputInformation.Count)
+                {
+                    UserInteraction(ref rowNumber, inputInformation, atmFunds);
+                    rowNumber++;
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO add something for the logger or similar. 
+            }
+
+            static List<string> DeserialiseJson(String path)
+            {
+                var inputJson = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<List<string>>(inputJson);
+            }
+
+            static void UserInteraction(ref int rowNumber, List<string> inputInformation, int atmFunds)
+            {
+                //TODO check line is not null
+                // TODO check split is not null
+                var rowInformationSplit = SplitRowInformation(inputInformation[rowNumber]);
+                if (rowInformationSplit[1] != rowInformationSplit[2])
+                {
+                    Console.WriteLine("ACCOUNT_ERR");
+                    return;
+                }
+                rowNumber++;
+                HandleUserOperations(ref rowNumber, inputInformation, ref atmFunds);
+            }
+
+            static void HandleUserOperations(ref int rowNumber, List<string> inputInformation, ref int atmFunds)
+            {
+                var rowInformationSplit = SplitRowInformation(inputInformation[rowNumber]);
+                var accoutBal = int.Parse(rowInformationSplit[0]);
+                var accountBalWithOverdraft = accoutBal + int.Parse(rowInformationSplit[1]);
+                rowNumber++;
+                while (rowNumber < inputInformation.Count)
+                {
+                    rowInformationSplit = SplitRowInformation(inputInformation[rowNumber]);
+
+                    switch (rowInformationSplit[0])
+                    {
+                        case "":
+                            return;
+                        case "B":
+                            Console.WriteLine(accoutBal);
+                            rowNumber++;
+                            break;
+                        case "W":
+                            var withdrawalAmount = int.Parse(rowInformationSplit[1]);              
+                            if (ValidTransaction(accountBalWithOverdraft, atmFunds, withdrawalAmount))
+                            {
+                                accoutBal = accoutBal - withdrawalAmount;
+                                atmFunds = atmFunds - withdrawalAmount;
+                                Console.WriteLine(accoutBal);
+                                rowNumber++;
+                                break;
+                            }
+                            rowNumber++;
+                            break;
+                    }
+                }
+            }
+
+            static bool ValidTransaction(int accountBalWithOverdraft, int atmFunds, int withdrawalAmount )
+            {
+                if (accountBalWithOverdraft < withdrawalAmount)
+                {
+                    Console.WriteLine("FUNDS_ERR");
+                    return false;
+                }else if(atmFunds< withdrawalAmount)
+                {
+                    Console.WriteLine("ATM_ERR");
+                    return false;
+                }
+                return true;
+            }
+
+            static string[] SplitRowInformation(string inputInformationRow)
+            {
+                return inputInformationRow.Split(" ");
+            }
         }
     }
 }
